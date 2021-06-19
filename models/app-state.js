1e2e693b5a9dc3c0d4d9c2b994cdb7e3126588f1
AndrePu <pugach@dlit.dp.ua>
2021-06-19 14:30:18 +0300

    Adding AppState class | Styling

---
+++ models/app-state.js
@@ -0,0 +1,8 @@
+export class AppState {
+    constructor() {
+        this.username = null;
+        this.interfaceLanguage = null;
+        this.fieldSize = null;
+        this.theme = null;
+    }
+}
